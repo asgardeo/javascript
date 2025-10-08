a71f79a37fda998f21dd61771b8feeeb4c4d70d2
Flaviano O. Silva <fosbsb@gmail.com>
2025-10-08 01:44:36 -0300

    feat(i18n): add Portuguese (pt-PT) translation, introduces the Portuguese (Portugal) language pack

--- packages/i18n/src/translations/index.ts
+++ packages/i18n/src/translations/index.ts
@@ -20,4 +20,5 @@ export {default as en_US} from './en-US';
 export {default as fr_FR} from './fr-FR';
 export {default as hi_IN} from './hi-IN';
 export {default as pt_BR} from './pt-BR';
+export {default as pt_PT} from './pt-PT';
 export {default as si_LK} from './si-LK';
