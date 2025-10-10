0d0950454a71c46ed16f8a3991c00fbb84e2a4da
auraofdivinity <auraofdivinity@gmail.com>
2025-10-10 14:05:08 +0530

    feat(storybooks): init storybooks

---
+++ packages/react/.storybook/preview.tsx
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import type {Preview} from '@storybook/react';
+import ThemeProvider from '../src/contexts/Theme/ThemeProvider';
+
+const preview: Preview = {
+  parameters: {
+    actions: {argTypesRegex: '^on[A-Z].*'},
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    Story => (
+      <ThemeProvider mode="light" inheritFromBranding={false}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default preview;
