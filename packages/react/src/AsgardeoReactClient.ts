293e87a94633070169e6d3117489e325f89fde93
NipuniBhagya <nipunib@wso2.com>
2026-03-31 12:55:32 +0530

    Fix eslint issue

--- packages/react/src/AsgardeoReactClient.ts
+++ packages/react/src/AsgardeoReactClient.ts
@@ -129,7 +129,8 @@ class AsgardeoReactClient<T extends AsgardeoReactConfig = AsgardeoReactConfig> e
       this.initializeConfig = {
         ...config,
         organizationHandle: resolvedOrganizationHandle,
-        periodicTokenRefresh: config?.tokenLifecycle?.refreshToken?.autoRefresh ?? (config as any)?.periodicTokenRefresh,
+        periodicTokenRefresh: config?.tokenLifecycle?.refreshToken?.autoRefresh 
+          ?? (config as any)?.periodicTokenRefresh,
       };
 
       return this.asgardeo.init(this.initializeConfig as any);
